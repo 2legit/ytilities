56fbc2be293ca03be3173b4e46086732fc9b9018
cag36 <yang@compareasiagroup.com>
2019-04-09 17:25:48 +0800

    3rd try at getting tests to work

--- tests/test_2.py
+++ tests/test_2.py
@@ -1,9 +1,9 @@
-from ytilities import *
+from ytilities import adwords
 
 def test_pass():
     assert True, "dummy sample test"
 
-def test_get_5():
+def test_get_5_pls():
     myclass = adwords.MyClass()
     assert myclass.get_5() == 5, "MyClass get_5 returns 5"
 
--- ytilities/__init__.py
+++ ytilities/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = '0.0.1'
 __author__ = 'Yang Gao <ygao78@gmail.com>'
-__all__ = ['adwords']
+__all__ = []
 
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
